0b6bd0e857073247ffff2ebad4420784eda22aad
Ross Rosen <rrosen326@gmail.com>
2021-01-11 11:05:02 -0800

    (split)Multiple
    * Autoclimate - bug that auto_off was required
    * dashboard_support: removed home_mode for seattle
    * gentle_wakeup: changed data for Kestrel

--- dashboard_support.py
+++ dashboard_support.py
@@ -20,8 +20,9 @@ class DashboardSupport(adplus.Hass):
 
     SCHEMA = {
         "test_mode": {"type": "boolean", "default": False, "required": False},
+        "debug": {"type": "boolean", "default": False, "required": False},
         "appname": {"required": False, "type": "string", "default": "dashboard_colors"},
-        "home_state_entity": {"required": True, "type": "string"},
+        "home_state_entity": {"required": False, "type": "string"},
         "app_state": {"required": True, "type": "string"},
         "climate": {
             "type": "dict",
@@ -43,6 +44,7 @@ class DashboardSupport(adplus.Hass):
         self.log("Initialize")
         self.argsn = adplus.normalized_args(self, self.SCHEMA, self.args, debug=False)
         self.test_mode = self.argsn.get("test_mode")
+        self.debug_mode = self.argsn.get("debug")
         self.appname = self.argsn["appname"]
         self.climates = self.argsn["climate"]["entities"]
         self.app_state_entity = self.argsn["app_state"]
@@ -52,7 +54,6 @@ class DashboardSupport(adplus.Hass):
             "climate.gym",
             "climate.tv_room",
         ]
-        self.home_state_entity = self.argsn["home_state_entity"]
 
         self.colors_dict: Dict[str, Optional[str]] = {
             climate: None for climate in self.climates
@@ -86,50 +87,54 @@ class DashboardSupport(adplus.Hass):
         The first arg will always be climate
         """
 
-        home_mode = self.get_state(self.home_state_entity)
-        if home_mode not in ["Home", "Away"]:
-            self.warn(f"Unexpected home_mode: {home_mode}")
-
         # Business logic
         color = None
         check = lambda service: self.call_service(
             f"autoclimate/{service}", climate=climate, namespace="autoclimate"
         )
 
-        if home_mode == "Home":
-            if check("is_offline"):
-                color = "yellow"
-            elif check("is_hardoff") and climate == "climate.cabin":
-                color = "orange"
-            elif check("is_on"):
-                if climate in ["climate.gym", "climate.tv_room"]:
-                    color = "red"
-                else:
-                    color = "green"
-            elif check("is_off"):
-                color = "white"
-            else:
-                self.warn(
-                    f"Unexpected state for climate: {climate}. State: {check('entity_state')}"
-                )
-                color = "purple"
-        elif home_mode == "Away":
-            if check("is_offline"):
-                color = "yellow"
-            elif check("is_hardoff") and climate == "climate.cabin":
-                color = "orange"
-            elif check("is_on"):
+        # Do not use "home_mode" in Seattle since that is SEATTLE's mode!
+        if check("is_offline"):
+            color = "yellow"
+        elif check("is_hardoff") and climate == "climate.cabin":
+            color = "orange"
+        elif check("is_on"):
+            if climate in ["climate.gym", "climate.tv_room"]:
                 color = "red"
-            elif check("is_off"):
-                color = "white"
             else:
-                self.warn(
-                    f"Unexpected state for climate: {climate}. State: {check('entity_state')}"
-                )
-                color = "purple"
+                color = "green"
+        elif check("is_off"):
+            color = "white"
+        else:
+            self.warn(
+                f"Unexpected state for climate: {climate}. State: {check('entity_state')}"
+            )
+            color = "purple"
+
 
         self.colors_dict[climate] = color
-        # self.log(f"{climate:35} -- color: {color}")
+        if self.debug_mode:
+            self.log(f"{climate:35} -- color: {color}")
         # Publish as flat state
         data = {climate: self.colors_dict[climate] for climate in self.climates}
+        self.set_state(f"app.{self.appname}", state="colors", attributes=data)
+
+        #
+        # Now do overall state
+        #
+        overall = self.get_state("app.autoclimate_state")
+        overall_color = None
+        if overall == 'offline':
+            overall_color = 'yellow'
+        elif overall == "on":
+            overall_color = "green"
+        elif overall == "off":
+            overall_color = "white"
+        else:
+            overall_color = "purple"
+
+        data["overall"] = overall_color
+        if self.debug_mode:
+            self.log(f"{'overall':35} -- color: {overall_color}")
+
         self.set_state(f"app.{self.appname}", state="colors", attributes=data)
