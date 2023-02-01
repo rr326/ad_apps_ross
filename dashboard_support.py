e76ea8749b5f16ba6f3a9099ff7a1d74f1810437
Ross Rosen <rrosen326@gmail.com>
2023-01-31 16:47:46 -0800

    removed debugging

--- dashboard_support.py
+++ dashboard_support.py
@@ -140,13 +140,9 @@ class DashboardSupport(adplus.Hass):
         color = "purple"  # default (error)
 
         def check(service: str):
-            retval = self.call_service(
+            return self.call_service(
                 f"autoclimate/{service}", climate=climate, return_result=True
             )
-            self.log(
-                f"**DEBUG: autoclimate/{service} for {climate} = |{retval}|, type: {type(retval)}. Home_mode = {self.get_state(self.haven_home_state_entity)}"
-            )
-            return retval
 
         home_mode = self.get_state(self.haven_home_state_entity)
 
